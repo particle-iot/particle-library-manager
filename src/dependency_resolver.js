1a514ff509e9b7c709e97d6d5cb4bae318890adf
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-14 17:26:12 +0200

    adds cloud library repository based on the v2 library support in particle-api-js

---
+++ src/dependency_resolver.js
@@ -0,0 +1,60 @@
+
+const semver = require('semver');
+
+export class Dependency {
+	constructor(name, version) {
+		this.name = name;
+		this.version = version;
+	}
+}
+
+
+/**
+ * Maintains state between invocations against a dependency resolver.
+ */
+export class DependencyResolverSession {
+
+	constrctor(libraryRepo) {
+		this.repo = libraryRepo;
+	}
+
+	/**
+	 * Fetches all dependencies of a given dependency.
+	 * @param dependency
+	 */
+	dependencies(dependency) {
+
+	}
+}
+
+
+
+/**
+ * Encapsulates the logic required to resolve dependencies.
+ */
+export class DependencyResolver {
+
+
+	/**
+	 *
+	 * @param session   The dependency resolution session
+	 * @param roots     The dependency roots.
+	 */
+	collectDependencies(session, roots) {
+		// iterate across the list of roots
+		// for each root, fetch the subdependencies and add any that don't already
+		// exist in the list to the end of the list.
+
+		// after this, the list may contain multiple versions for a given library
+
+	}
+
+	resolveDependencies(session, dependencies) {
+		// the dependencies are bucketed by library and version
+		// then each version reduced to a single item (the most recent)
+
+		// this provides the final list of dependencies for the given set of roots
+	}
+
+}
+
