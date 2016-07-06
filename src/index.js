1aa04f56f038c986b72e0c1cb11dea38947fa448
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-07-06 23:08:50 +0200

    provide root directory of package so downstream pcakages can resolve library resources

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@
 export * from './librepo';
 export * from './librepo_build';
 export * from './librepo_fs';
+
+const path = require('path');
+const appRoot = require('app-root-path').toString();
+
+function resourcesDir() {
+	return path.join(appRoot, 'resources');
+}
+
+export {
+	resourcesDir
+};
