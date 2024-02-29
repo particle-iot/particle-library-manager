aab33b9fad1f22e292b2afda3597cd42a867fe21
Hugo Montero <hugo.montero.ventura@particle.io>
2024-02-29 15:17:58 -0600

    fix tests according the new class structure

--- test/libinit.spec.js
+++ test/libinit.spec.js
@@ -20,16 +20,13 @@
 import { expect, sinon } from './test-setup';
 
 const fs = require('fs');
+const fsExtra = require('fs-extra');
 const path = require('path');
-const fse = require('fs-extra');
 const timekeeper = require('timekeeper');
 
 
 import { LibraryInitGenerator } from '../src/libinit';
-import { appRoot } from '../src/index';
-
-
-
+const TEMP_PATH = path.join(__dirname, '../', 'tmp', 'library');
 
 describe('library initialize', function doit() {
 	const testData = { name: 'nominative', version: '1.2.3', author: 'Borges <borges@example.com>' };
@@ -38,8 +35,9 @@ describe('library initialize', function doit() {
 		timekeeper.freeze(Date.parse('2015-12-15'));
 	});
 
-	after(function releaseTime() {
+	after(async function releaseTime() {
 		timekeeper.reset();
+		await fsExtra.remove(TEMP_PATH);
 	});
 
 	/**
@@ -54,7 +52,8 @@ describe('library initialize', function doit() {
 			actual = expected;
 		}
 		const expectedContent = fs.readFileSync(path.join(__dirname,'./generator/', expected), 'utf8');
-		assert.fileContent(actual, expectedContent);
+		const actualContent = fs.readFileSync(path.join(TEMP_PATH, actual), 'utf8');
+		expect(expectedContent).to.equal(actualContent);
 	}
 
 	/**
@@ -70,43 +69,28 @@ describe('library initialize', function doit() {
 		assertGeneratedContent('examples/usage/usage.ino');
 	}
 
-	/**
-	 * Creates a LibraryInitGenerator and makes the source content equal to the
-	 * `src/init` directory. The generator is run in a temporary directory.
-	 * @param {string} dir       The directory under the source folder that contains the generator sources
-	 * @param {function} cb      A callback that is passed the created generator.
-	 * @returns {Promise}   To run the generator.
-	 */
-	function generator(dir, cb) {
-		let result = helpers.run(buildLibraryInitGeneratorClass());
-		if (dir) {
-			result = result.inTmpDir((tmpdir) => {
-				// `tmpdir` is the path to the new temporary directory
-				fse.copySync(path.join(appRoot, 'src', dir), tmpdir);
-			});
-		}
-		if (cb) {
-			result = cb(result);
-		}
-		return result.toPromise();
-	}
-
 	describe('generator', () => {
-		it.skip('interpolates all template files', function doit() {
+		const prompt = sinon.stub().resolves();
+
+		it('interpolates all template files', async () => {
 			this.timeout(30000);
-			return generator('init', (result) => {
-				return result.withOptions(testData);       // Mock options passed in
-			}).then(validateOutput);
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
+			sut._destinationRoot = TEMP_PATH;
+			await sut.run({ options: testData });
+			expect(sut.prompt).to.have.been.calledWith([]);
+			validateOutput();
 		});
 
-		it.skip('should prompt for all properties if not provided', () => {
-			return generator('init', (result) => {
-				return result.withPrompts(testData);       // Mock options passed in
-			}).then(validateOutput);
+		it('should prompt for all properties if not provided', async () => {
+			this.timeout(30000);
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves(testData) });
+			sut._destinationRoot = TEMP_PATH;
+			await sut.run();
+			validateOutput();
 		});
 
 		it('sets the year from the "year" options', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			sut.options = { year: 1234 };
 			const date = new Date();
 			sut._setYear(date);
@@ -114,7 +98,7 @@ describe('library initialize', function doit() {
 		});
 
 		it('sets the year from the current year when "year" option not defined', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			sut.options = {};
 			const date = new Date();
 			sut._setYear(date);
@@ -123,7 +107,7 @@ describe('library initialize', function doit() {
 
 
 		it('sets the output directory from the "dir" option', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			sut.options = { dir: 'abcd' };
 			sut.destinationRoot = sinon.stub();
 			sut._setOutputDir();
@@ -131,7 +115,7 @@ describe('library initialize', function doit() {
 		});
 
 		it('does not set the output directory when the dir option is not present', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			sut.options = { };
 			sut.destinationRoot = sinon.stub();
 			sut._setOutputDir();
@@ -139,21 +123,21 @@ describe('library initialize', function doit() {
 		});
 
 		it('sets the name_code option to the code-safe name with the first letter lowercased', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			sut.options = {};
 			sut._handlePrompts({ name:'SparkLib++' });
 			expect(sut.options).to.have.property('name_code').equal('sparkLib');
 		});
 
 		it('sets the Name_code option to the code-safe name with first letter capitalized', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			sut.options = {};
 			sut._handlePrompts({ name:'my-lib++' });
 			expect(sut.options).to.have.property('Name_code').equal('Mylib');
 		});
 
 		it('does not set the Name_code option when name is not present', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			sut.options = {};
 			sut._handlePrompts({ name2:'abcd' });
 			expect(sut.options).to.not.have.property('Name_code');
@@ -163,56 +147,56 @@ describe('library initialize', function doit() {
 			const nameError = 'name: must only contain letters, numbers, dashes, underscores and plus signs';
 			const versionError = 'version: must be formatted like 1.0.0';
 			it('validates the name', () => {
-				const sut = new LibraryInitGenerator();
+				const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 				sut.options = {};
 				expect(() => sut._handlePrompts({ name:'ab/cd' })).to.throw(nameError);
 			});
 
 			it('validates the version', () => {
-				const sut = new LibraryInitGenerator();
+				const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 				sut.options = {};
 				expect(() => sut._handlePrompts({ version:'ab/cd' })).to.throw(versionError);
 			});
 
 			it('validates the author, which is freeform', () => {
-				const sut = new LibraryInitGenerator();
+				const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 				sut.options = {};
 				expect(() => sut._handlePrompts({ author:'ab/cd' })).to.not.throw();
 			});
 
 			it('validates the initial name value', () => {
-				const sut = new LibraryInitGenerator();
+				const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 				sut.options = { name: '//' };
 				expect(() => sut._checkFields()).to.throw(nameError);
 			});
 
 			it('validates the initial version value', () => {
-				const sut = new LibraryInitGenerator();
+				const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 				sut.options = { version: '//' };
 				expect(() => sut._checkFields()).to.throw(versionError);
 			});
 
 			it('validates the initial version value when set as a number', () => {
-				const sut = new LibraryInitGenerator();
+				const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 				sut.options = { version: 123 };
 				expect(() => sut._checkFields()).to.throw(versionError);
 			});
 
 
 			it('validates the prompts', () => {
-				const sut = new LibraryInitGenerator();
+				const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 				sut.options = {};
 				const prompts = sut._allPrompts();
 				expect(prompts).has.property('length').equal(3);
 
-				expect(prompts[0].validate('ab/cd')).to.equal(nameError+'.');
+				expect(prompts[0].validate('ab/cd')).to.equal(nameError + '.');
 				expect(prompts[1].validate('ab/cd')).to.equal(versionError);
 				expect(prompts[2].validate('ab/cd')).to.be.true;
 			});
 		});
 
 		it('the _prompt method configures and fetches options', () => {
-			const sut = new LibraryInitGenerator();
+			const sut = new LibraryInitGenerator({ prompt: prompt.resolves() });
 			// given
 			sut._setYear = sinon.stub();
 			sut._setOutputDir = sinon.stub();
